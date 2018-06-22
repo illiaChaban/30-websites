f836edc251ebd6472c796a89fbc78b010ff88dbf
Illia Chaban <illia.chaban.8@gmail.com>
2018-06-22 15:13:25 -0400

    added description on the main page, started working on the menu

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 let links = document.querySelectorAll('li');
 
-// links.forEach(link => link.querySelector('.screenshots').style.display = 'inline');
-
 for ( let link of links) {
     let screenshots = link.querySelector('.screenshots');
+    let description = link.querySelector('.description');
     link.addEventListener( 'mouseover', () => {
         if (screenshots) screenshots.style.display = 'inline';
+        if (description) description.style.display = 'inline'
     })
-    link.addEventListener( 'mouseout', (e) => {
+    link.addEventListener( 'mouseout', () => {
         if (screenshots) screenshots.style.display = 'none';
+        if (description) description.style.display = 'none'
     })
 }
-
 
