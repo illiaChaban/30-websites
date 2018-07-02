b0867a9175c8912bb530235c0e41decef88a63ee
Illia Chaban <illia.chaban.8@gmail.com>
2018-07-02 14:32:29 -0400

    added transitions to images

--- menu/main.js
+++ menu/main.js
@@ -8,14 +8,13 @@ document
     .forEach( (menu, i) => {
         let firstLi = menu.querySelector('li');
         focusedElements.push(firstLi);
-        let img = document.querySelectorAll('.image > img')[i];
-        img.src = `./images/${firstLi.querySelector('h3').textContent.match(getImageName)[0]}.jpg`;
+        menu.querySelector('.image').style.backgroundImage = `url('./images/${firstLi.querySelector('h3').textContent.match(getImageName)[0]}.jpg')`
         firstLi.className = 'focused whiteText'
         menu.querySelectorAll('li')
             .forEach( li => {
                 li.addEventListener( 'click', () => {
                     let title = li.querySelector('h3').textContent.match(getImageName)[0];
-                    img.src = `./images/${title}.jpg`;
+                    menu.querySelector('.image').style.backgroundImage = `url('./images/${title}.jpg')`
                     focusedElements[i].className = '';
                     focusedElements[i] = li;
                     focusedElements[i].className = 'focused whiteText'
