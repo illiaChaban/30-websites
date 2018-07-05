66b054d5f868e3528d4a936ad5875c60cf664f20
Illia Chaban <illia.chaban.8@gmail.com>
2018-07-04 20:03:05 -0400

    bug fix

--- menu+threeJS/main.js
+++ menu+threeJS/main.js
@@ -3,34 +3,7 @@ const height = 100;
 const getImageName = /([a-z]+-*[" "]*)+[a-z]/i
 let focusedElements = [];
 
-
-document.querySelectorAll('.menu').forEach( (menu, i) => {
-    let firstLi = menu.querySelector('li');
-    focusedElements.push(firstLi);
-    firstLi.className = 'focused whiteText'
-
-    let initialTitle = getTitle(firstLi);
-    let root = getRoot(i);
-    let initialSlide = createSlide(initialTitle, true);
-    root.scene.add(initialSlide);
-
-    menu.querySelectorAll('li')
-        .forEach( li => {
-            let title = getTitle(li)
-            li.addEventListener( 'click', () => {
-                let slide = createSlide(title);
-                root.scene.add(slide);
-                slide.transition();
-
-                focusedElements[i].className = '';
-                focusedElements[i] = li;
-                focusedElements[i].className = 'focused whiteText'
-            })
-        })
-})
-
-
-getRoot = i => {
+let getRoot = i => {
   let root = new THREERoot({
     createCameraControls: !true,
     antialias: (window.devicePixelRatio === 1),
@@ -44,12 +17,12 @@ getRoot = i => {
   return root;
 }
 
-getTitle = (liElement) => {
+let getTitle = (liElement) => {
   let title = liElement.querySelector('h3').textContent.match(getImageName)[0];
   return title.split(' ').join('+');
 }
 
-createSlide = (imgTitle, initial = false) => {
+let createSlide = (imgTitle, initial = false) => {
   let phase = initial ? 'out' : 'in';
   let slide = new Slide(width, height, phase);
   var l1 = new THREE.ImageLoader();
@@ -59,5 +32,30 @@ createSlide = (imgTitle, initial = false) => {
   })
   return slide;
 }
+
+document.querySelectorAll('.menu').forEach( (menu, i) => {
+  let firstLi = menu.querySelector('li');
+  focusedElements.push(firstLi);
+  firstLi.className = 'focused whiteText'
+
+  let initialTitle = getTitle(firstLi);
+  let root = getRoot(i);
+  let initialSlide = createSlide(initialTitle, true);
+  root.scene.add(initialSlide);
+
+  menu.querySelectorAll('li')
+      .forEach( li => {
+          let title = getTitle(li)
+          li.addEventListener( 'click', () => {
+              let slide = createSlide(title);
+              root.scene.add(slide);
+              slide.transition();
+
+              focusedElements[i].className = '';
+              focusedElements[i] = li;
+              focusedElements[i].className = 'focused whiteText'
+          })
+      })
+})
 
 
