df39995d02e9b8e04b1e61d842cd811d4a6c013c
Illia Chaban <illia.chaban.8@gmail.com>
2019-02-27 23:44:25 -0500

    added slider resizing functionality based on the image aspect ratio, to do: 1) style arrows, 2) add bullets at the bottom, so that user can navigate to any image + css animation on slide change on those bullets

--- slider/main.js
+++ slider/main.js
@@ -11,28 +11,68 @@ class Slider {
         this.lastSliderPosition = null;
     }
 
-    sizeSlides() {
+    init() {
+        this.$slider.classList.add("slider", "activated", "center"); // activating CSS rules
+        this.resizeSlider();
+        this.bindSliderResizing();
+        // this.indexSlides();
+        this.bindDragDropSlides();
+        this.createArrows();
+        console.log("Slider was initialized")
+    };
+
+    resizeSlider() {
+        // updating Slider height based on natural aspect ratio
+        // of the first image and current slider's width
+        // ---- using the first image as a template for now
+        // ---- ideally should get average aspect ratio from all images
+        // checking if image has already loaded
+        let loadedImg = new Promise( (resolve, reject) => {
+            let img = this.$slides[0].querySelector("img");
+            if ( img.complete ) {
+                resolve( {img, status: "complete"} )
+            } else {
+                img.addEventListener( "load", () => resolve( {img, status: "loaded"} ))
+            }
+        })
+        loadedImg.then( ({img, status}) => {
+            let aspectRatio = this.getImgNaturalAspectRatio( img );
+            this.adjustSliderHeight( aspectRatio );
+            this.resizeSlides();
+            // console.log("slider Resized", {status});
+        })
+    }
+
+    getImgNaturalAspectRatio( img ) {
+        let width = img.naturalWidth;
+        let height = img.naturalHeight;
+        let aspectRatio = width / height;
+        return aspectRatio;
+    };
+
+    adjustSliderHeight( aspectRatio ) {
+        this.updateSliderDimensionsInfo();
+        let currWidth = this.sliderRect.width;
+        let newHeight = currWidth / aspectRatio;
+        this.$slidesWrap.style.height = newHeight + "px";
+        this.updateSliderDimensionsInfo();
+    };
+
+    updateSliderDimensionsInfo() {
+        this.sliderRect = this.$slidesWrap.getBoundingClientRect();
+    }
+
+    resizeSlides() {
         let {width, height} = this.sliderRect;
         this.$slides.forEach( slide => {
             slide.setAttribute("style", `width: ${width}px; height: ${height}px`);
         });
     };
 
-    init() {
-        this.$slider.classList.add("slider", "activated", "center"); // activating CSS rules
-        this.sliderRect = this.$slidesWrap.getBoundingClientRect(); // getting new measurements
-        this.sizeSlides();
-        // this.indexSlides();
-        this.bindDragDropSlides();
-        this.createArrows();
-        console.log("Slider was initialized")
-    };
-    
-    sizeSlides() {
-        let {width, height} = this.sliderRect;
-        this.$slides.forEach( slide => {
-            slide.setAttribute("style", `width: ${width}px; height: ${height}px`);
-        })
+
+    bindSliderResizing() {
+        window.addEventListener( "resize", e => this.resizeSlider() );
+        document.addEventListener( "fullscreenchange", e => resizeSlider() );
     };
     
     indexSlides() {
